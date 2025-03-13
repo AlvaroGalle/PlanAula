2dfa83896c70eff31a2d00471835737e2500dab7
51178456 <agallego@ccoo.es>
2025-03-13 13:28:51 +0100

    aa

--- PlanAula/src/main/java/com/example/planaula/controllers/GuardiasController.java
+++ PlanAula/src/main/java/com/example/planaula/controllers/GuardiasController.java
@@ -5,6 +5,7 @@ import com.example.planaula.services.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,6 +18,8 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/guardias")
 public class GuardiasController {
+
+    private final String RUTATEMPLATE = "guardias/";
 
     private final GuardiasService guardiasService;
     private final HorasService horasService;
@@ -32,61 +35,107 @@ public class GuardiasController {
 
     @GetMapping("")
     public String findAllGuardias(@RequestParam(required = false, defaultValue = "0") int dia,
-                                                @RequestParam(required = false, defaultValue = "0") int hora,
-                                                @RequestParam(required = false, defaultValue = "0") String turno,
-                                                @RequestParam(required = false, defaultValue = "0") int profesor,
-                                                Model model) {
+                                  @RequestParam(required = false, defaultValue = "0") int hora,
+                                  @RequestParam(required = false, defaultValue = "0") String turno,
+                                  @RequestParam(required = false, defaultValue = "0") int profesor,
+                                  Model model) {
         model.addAttribute("diaFiltro", dia);
         model.addAttribute("horaFiltro", hora);
         model.addAttribute("turnoFiltro", turno);
         model.addAttribute("profesorFiltro", profesor);
 
-        List<DiaDTO> diaDTOList =  diasService.findAllDias();
+        List<DiaDTO> diaDTOList = diasService.findAllDias();
         model.addAttribute("dias", diaDTOList);
 
-        List<HoraDTO> horaDTOList =  horasService.findAllHoras();
+        List<HoraDTO> horaDTOList = horasService.findAllHoras();
         model.addAttribute("horas", horaDTOList);
 
-        List<ProfesorDTO> profesorDTOList =  profesoresService.findAllProfesores();
+        List<ProfesorDTO> profesorDTOList = profesoresService.findAllProfesores();
         model.addAttribute("profesores", profesorDTOList);
-        for(ProfesorDTO p : profesorDTOList){
-            if(p.getId() == profesor){
+        for (ProfesorDTO p : profesorDTOList) {
+            if (p.getId() == profesor) {
                 model.addAttribute("profesor", p);
             }
         }
 
-        List<GuardiasDTO> guardiasDTOList = guardiasService.findAllGuardiasByDiaAndHoraAndProfesor(dia,hora,profesor);
+        List<GuardiasDTO> guardiasDTOList = guardiasService.findAllGuardiasByDiaAndHoraAndProfesor(dia, hora, profesor);
 
-        Map<String, String> mapaTurnos = Map.of(
-                "G1", "guardia1", "G2", "guardia2", "G3", "guardia3",
-                "L1", "libranza1", "L2", "libranza2", "L3", "libranza3",
-                "R1", "recreo1", "R2", "recreo2", "R3", "recreo3"
-        );
+        Map<String, String> mapaTurnos = this.mapaTurnos();
         model.addAttribute("turnos", mapaTurnos);
 
-        if(!guardiasDTOList.isEmpty() && !Objects.equals(turno, "0")) {
+        if (!guardiasDTOList.isEmpty() && !Objects.equals(turno, "0")) {
             String campo = mapaTurnos.getOrDefault(turno, "");
 
             guardiasDTOList = guardiasDTOList.stream()
                     .filter(guardia -> !estaVacio(obtenerValorCampoDinamico(guardia, campo)))
                     .collect(Collectors.toList());
         }
-        model.addAttribute("page",guardiasDTOList);
+        model.addAttribute("page", guardiasDTOList);
         return "guardias";
     }
 
-        private String obtenerValorCampoDinamico(GuardiasDTO guardia, String nombreCampo) {
-            try {
-                Field campo = GuardiasDTO.class.getDeclaredField(nombreCampo);
-                campo.setAccessible(true);
-                Object valor = campo.get(guardia);
-                return valor != null ? valor.toString() : null;
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                return null;
-            }
-        }
+    @GetMapping("{id}")
+    public String findGuardiaById(@PathVariable int id, Model model) {
+        try {
+            GuardiasDTO guardiasDTO = guardiasService.findGuardiaById(id);
+            model.addAttribute("guardia", guardiasDTO);
 
-        private boolean estaVacio(String valor) {
-            return valor == null || valor.trim().isEmpty();
+            List<ProfesorDTO> profesorDTOList = profesoresService.findAllProfesores();
+            model.addAttribute("profesores", profesorDTOList);
+
+            Map<String, String> mapaTurnos = this.mapaTurnos();
+            model.addAttribute("turnos", mapaTurnos);
+            return RUTATEMPLATE + "cardGuardias";
+        } catch (Exception e) {
+            return e.getMessage();
         }
+    }
+
+    @GetMapping("accion")
+    public String accionesGuardias(@RequestParam String accion,
+                                   @RequestParam String turno,
+                                   @RequestParam Integer profesor,
+                                   @RequestParam Integer id) {
+        try {
+            Map<String, String> mapeoTurnos = Map.of(
+                    "guardia1", "guardia 1",
+                    "guardia2", "guardia 2",
+                    "guardia3", "guardia 3",
+                    "libranza1", "libranza 1",
+                    "libranza2", "libranza 2",
+                    "libranza3", "libranza 3",
+                    "recreo1", "recreo1",
+                    "recreo2", "recreo2",
+                    "recreo3", "recreo3"
+            );
+            guardiasService.accionGuardias(accion, turno, profesor, id);
+            return "redirect:/guardias/" + id;
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    private Map<String, String> mapaTurnos(){
+       return Map.of(
+               "G1", "guardia1", "G2", "guardia2", "G3", "guardia3",
+               "L1", "libranza1", "L2", "libranza2", "L3", "libranza3",
+               "R1", "recreo1", "R2", "recreo2", "R3", "recreo3"
+       );
+    }
+
+    private String obtenerValorCampoDinamico(GuardiasDTO guardia, String nombreCampo) {
+        try {
+            Field campo = GuardiasDTO.class.getDeclaredField(nombreCampo);
+            campo.setAccessible(true);
+            Object valor = campo.get(guardia);
+            return valor != null ? valor.toString() : null;
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return null;
+        }
+    }
+
+    private boolean estaVacio(String valor) {
+        return valor == null || valor.trim().isEmpty();
+    }
+
 }
--- PlanAula/src/main/java/com/example/planaula/services/GuardiasService.java
+++ PlanAula/src/main/java/com/example/planaula/services/GuardiasService.java
@@ -3,6 +3,7 @@ package com.example.planaula.services;
 import com.example.planaula.Dto.AsignaturaDTO;
 import com.example.planaula.Dto.GuardiasDTO;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -74,4 +75,61 @@ public class GuardiasService {
                 ))
                 .collect(Collectors.toList());
     }
+
+    public GuardiasDTO findGuardiaById(Integer id) {
+        String sql = "SELECT g.id, d.campo1, h.campo1, \n" +
+        "       a1.campo1 AS Aula_1A, a2.campo1 AS Aula_1B, \n" +
+                "       a3.campo1 AS Aula_2A, a4.campo1 AS Aula_2B, \n" +
+                "       a5.campo1 AS Aula_3A, a6.campo1 AS Aula_3B, \n" +
+                "       a7.campo1 AS Aula_4A, a8.campo1 AS Aula_4B, \n" +
+                "       a9.campo1 AS Aula_1CC \n" +
+                "FROM `guardias 2425` g \n" +
+                "INNER JOIN dias d ON g.Día = d.id \n" +
+                "INNER JOIN horas h ON g.Hora = h.id \n" +
+                "LEFT JOIN `profes 2425` a1 ON g.`guardia 1` = a1.id \n" +
+                "LEFT JOIN `profes 2425` a2 ON g.`guardia 2` = a2.id \n" +
+                "LEFT JOIN `profes 2425` a3 ON g.`guardia 3` = a3.id \n" +
+                "LEFT JOIN `profes 2425` a4 ON g.`libranza 1` = a4.id \n" +
+                "LEFT JOIN `profes 2425` a5 ON g.`libranza 2` = a5.id \n" +
+                "LEFT JOIN `profes 2425` a6 ON g.`libranza 3` = a6.id \n" +
+                "LEFT JOIN `profes 2425` a7 ON g.recreo1 = a7.id \n" +
+                "LEFT JOIN `profes 2425` a8 ON g.recreo2 = a8.id \n" +
+                "LEFT JOIN `profes 2425` a9 ON g.recreo3 = a9.id \n" +
+                "WHERE 1=1 \n" +
+                "AND (g.id = :id)";
+
+        Object[] resultado = (Object[]) entityManager.createNativeQuery(sql)
+                .setParameter("id", id)
+                .getSingleResult();
+
+        return new GuardiasDTO(
+                        ((Number) resultado[0]).intValue(),
+                        (String) resultado[1],
+                        (String) resultado[2],
+                        (String) resultado[3],
+                        (String) resultado[4],
+                        (String) resultado[5],
+                        (String) resultado[6],
+                        (String) resultado[7],
+                        (String) resultado[8],
+                        (String) resultado[9],
+                        (String) resultado[10],
+                        (String) resultado[11]
+                );
+    }
+
+    @Transactional
+    public void accionGuardias(String accion, String turno, Integer profesor, Integer id) {
+        String sql = "UPDATE `guardias 2425` SET `" + turno + "` = " +
+                "CASE WHEN :accion = 'A' OR :accion = 'M' THEN :profesor " +
+                "WHEN :accion = 'E' THEN NULL ELSE `" + turno + "` END " +
+                "WHERE id = :id";
+
+        Object[] resultado = (Object[]) entityManager.createNativeQuery(sql)
+                .setParameter("accion", accion)
+                .setParameter("profesor", profesor)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+
 }
