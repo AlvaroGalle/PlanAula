ca4b9d9796d42aa4d3c829cb4598814e10d7b9a7
AlvaroGalle <alvarogy2004@gmail.com>
2025-03-16 18:55:52 +0100

    select card espacios

--- PlanAula/src/main/java/com/example/planaula/services/EspaciosService.java
+++ PlanAula/src/main/java/com/example/planaula/services/EspaciosService.java
@@ -101,32 +101,67 @@ public class EspaciosService {
     }
 
     public EspacioDTO findEspacioById(Integer id) {
-       /* String sql = "SELECT g.id, d.campo1, h.campo1, \n" +
-        "       a1.campo1 AS Aula_1A, a2.campo1 AS Aula_1B, \n" +
-                "       a3.campo1 AS Aula_2A, a4.campo1 AS Aula_2B, \n" +
-                "       a5.campo1 AS Aula_3A, a6.campo1 AS Aula_3B, \n" +
-                "       a7.campo1 AS Aula_4A, a8.campo1 AS Aula_4B, \n" +
-                "       a9.campo1 AS Aula_1CC \n" +
-                "FROM `guardias 2425` g \n" +
-                "INNER JOIN dias d ON g.Día = d.id \n" +
-                "INNER JOIN horas h ON g.Hora = h.id \n" +
-                "LEFT JOIN `profes 2425` a1 ON g.`guardia 1` = a1.id \n" +
-                "LEFT JOIN `profes 2425` a2 ON g.`guardia 2` = a2.id \n" +
-                "LEFT JOIN `profes 2425` a3 ON g.`guardia 3` = a3.id \n" +
-                "LEFT JOIN `profes 2425` a4 ON g.`libranza 1` = a4.id \n" +
-                "LEFT JOIN `profes 2425` a5 ON g.`libranza 2` = a5.id \n" +
-                "LEFT JOIN `profes 2425` a6 ON g.`libranza 3` = a6.id \n" +
-                "LEFT JOIN `profes 2425` a7 ON g.recreo1 = a7.id \n" +
-                "LEFT JOIN `profes 2425` a8 ON g.recreo2 = a8.id \n" +
-                "LEFT JOIN `profes 2425` a9 ON g.recreo3 = a9.id \n" +
-                "WHERE 1=1 \n" +
-                "AND (g.id = :id)";
+       String sql = "SELECT e.id, d.campo1, h.campo1, \n" +
+               "       a1.campo1 AS Aula_1A, a2.campo1 AS Aula_1B, \n" +
+               "       a3.campo1 AS Aula_2A, a4.campo1 AS Aula_2B, \n" +
+               "       a5.campo1 AS Aula_3A, a6.campo1 AS Aula_3B, \n" +
+               "       a7.campo1 AS Aula_4A, a8.campo1 AS Aula_4B, \n" +
+               "       a9.campo1 AS Aula_1CC, a10.campo1 AS Aula_1SS, \n" +
+               "       a11.campo1 AS Lab_CC, a12.campo1 AS Lab_Id, \n" +
+               "       a13.campo1 AS Desdoble_A, a14.campo1 AS Desdoble_B, \n" +
+               "       a15.campo1 AS Desdoble_C, a16.campo1 AS A_ENLACE, \n" +
+               "       a17.campo1 AS Bajo_Patio, a18.campo1 AS Bajo_Espejos \n" +
+               "FROM `espacios 2425` e \n" +
+               "INNER JOIN dias d ON e.Día = d.id \n" +
+               "INNER JOIN horas h ON e.Hora = h.id \n" +
+               "LEFT JOIN asignaturas a1 ON e.`Aula 1A` = a1.id \n" +
+               "LEFT JOIN asignaturas a2 ON e.`Aula 1B` = a2.id \n" +
+               "LEFT JOIN asignaturas a3 ON e.`Aula 2A` = a3.id \n" +
+               "LEFT JOIN asignaturas a4 ON e.`Aula 2B` = a4.id \n" +
+               "LEFT JOIN asignaturas a5 ON e.`Aula 3A` = a5.id \n" +
+               "LEFT JOIN asignaturas a6 ON e.`Aula 3B` = a6.id \n" +
+               "LEFT JOIN asignaturas a7 ON e.`Aula 4A` = a7.id \n" +
+               "LEFT JOIN asignaturas a8 ON e.`Aula 4B` = a8.id \n" +
+               "LEFT JOIN asignaturas a9 ON e.`Aula 1CC` = a9.id \n" +
+               "LEFT JOIN asignaturas a10 ON e.`Aula 1SS` = a10.id \n" +
+               "LEFT JOIN asignaturas a11 ON e.`Lab CC` = a11.id \n" +
+               "LEFT JOIN asignaturas a12 ON e.`Lab Id` = a12.id \n" +
+               "LEFT JOIN asignaturas a13 ON e.`Desdoble A` = a13.id \n" +
+               "LEFT JOIN asignaturas a14 ON e.`Desdoble B` = a14.id \n" +
+               "LEFT JOIN asignaturas a15 ON e.`Desdoble C` = a15.id \n" +
+               "LEFT JOIN asignaturas a16 ON e.`A ENLACE` = a16.id \n" +
+               "LEFT JOIN asignaturas a17 ON e.`Bajo Patio` = a17.id \n" +
+               "LEFT JOIN asignaturas a18 ON e.`Bajo Espejos` = a18.id \n" +
+               "WHERE 1=1 \n" +
+               "AND (e.id = :id)";
 
         Object[] resultado = (Object[]) entityManager.createNativeQuery(sql)
                 .setParameter("id", id)
                 .getSingleResult();
-        */
+        
 
-        return new EspacioDTO();
+        return new EspacioDTO(((Number) resultado[0]).intValue(), 
+                (String) resultado[1],
+                (String) resultado[2],
+                (String) resultado[3],
+                (String) resultado[4],
+                (String) resultado[5],
+                (String) resultado[6],
+                (String) resultado[7],
+                (String) resultado[8],
+                (String) resultado[9],
+                (String) resultado[10],
+                (String) resultado[11],
+                (String) resultado[12],
+                (String) resultado[13],
+                (String) resultado[14],
+                (String) resultado[15],
+                (String) resultado[16],
+                (String) resultado[17],
+                (String) resultado[18],
+                (String) resultado[19],
+                (String) resultado[20]);
+
+
     }
 }
